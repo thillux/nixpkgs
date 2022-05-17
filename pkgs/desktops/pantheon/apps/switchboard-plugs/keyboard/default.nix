3dd67c4736a70661f679302e282190cf1f86687e
Bobby Rong <rjl931189261@126.com>
2022-05-17 22:09:15 +0800

    pantheon.switchboard-plug-keyboard: 2.7.0 -> 3.1.0

--- pkgs/desktops/pantheon/apps/switchboard-plugs/keyboard/default.nix
+++ pkgs/desktops/pantheon/apps/switchboard-plugs/keyboard/default.nix
@@ -23,20 +23,20 @@
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-keyboard";
-  version = "2.7.0";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ge87rctbd7iR9x9Xq4sMIC09DiPHbpbWBgMZUuJNWbw=";
+    sha256 = "sha256-OMcySVnOkXy236YRldVK1hB1zCWMBhcGnCsqQOU08wo=";
   };
 
   patches = [
     ./0001-Remove-Install-Unlisted-Engines-function.patch
     (substituteAll {
       src = ./fix-paths.patch;
-      inherit ibus onboard;
+      inherit ibus onboard libgnomekbd;
     })
   ];
 
@@ -55,7 +55,6 @@ stdenv.mkDerivation rec {
     gtk3
     ibus
     libgee
-    libgnomekbd
     libhandy
     libxklavier
     switchboard
